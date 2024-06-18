826fab166e9127cd5ba61daa7431b595e8f4a45d
Zack9961 <mrzack9099@gmail.com>
2024-06-18 21:07:12 +0200

    Pulizia codice

--- loader.py
+++ loader.py
@@ -7,13 +7,6 @@ from torch_geometric.transforms import RandomNodeSplit
 def load_data(data_path, noAgg=False):
 
     # Read edges, features and classes from csv files
-    # df_edges = pd.read_csv(osp.join(data_path, "elliptic_txs_edgelist.csv"))
-    # df_features = pd.read_csv(osp.join(data_path, "elliptic_txs_features.csv"), header=None)
-    # df_classes = pd.read_csv(osp.join(data_path, "elliptic_txs_classes.csv"))
-    # print(df_edges)
-    # print(df_features)
-    # print(df_classes)
-
     df_edges = pd.read_csv(osp.join(data_path, "edges.csv"), usecols=["clId1","clId2"])
     df_features = pd.read_csv(osp.join(data_path, "background_nodes.csv"), nrows=1000000)
     df_classes_raw1 = pd.read_csv(osp.join(data_path, "connected_components.csv"))
@@ -21,49 +14,15 @@ def load_data(data_path, noAgg=False):
     df_classes = pd.merge(df_classes_raw1, df_classes_raw2, on='ccId')
     df_classes = df_classes.drop('ccId', axis=1)
     
-    print(df_edges)
-    print(df_features)
-    print(df_classes)
-
-    # Name colums basing on index
-    # colNames1 = {'0': 'txId', 1: "Time step"}
-    # colNames2 = {str(ii+2): "Local_feature_" + str(ii+1) for ii in range(94)}
-    # colNames3 = {str(ii+96): "Aggregate_feature_" + str(ii+1) for ii in range(72)}
-
-    # colNames = dict(colNames1, **colNames2, **colNames3)
-    # colNames = {int(jj): item_kk for jj, item_kk in colNames.items()}
-
-    # Rename feature columns
-    # df_features = df_features.rename(columns=colNames)
-    # if noAgg:
-    #     df_features = df_features.drop(df_features.iloc[:, 96:], axis = 1)
-
-    # Map unknown class to '3'
-    #df_classes.loc[df_classes['class'] == 'unknown', 'class'] = '3'
-
-    # Map ccLabel in numeric value
-    #df_classes.loc[df_classes['ccLabel'] == 'licit', 'ccLabel'] = '0'
-    #df_classes.loc[df_classes['ccLabel'] == 'suspicious', 'ccLabel'] = '1'
-
     # Merge classes and features in one Dataframe
     df_class_feature = pd.merge(df_classes, df_features)
 
-    # Exclude records with unknown class transaction
-    #df_class_feature = df_class_feature[df_class_feature["class"] != '3']
-
-    # Build Dataframe with head and tail of transactions (edges)
-    # known_txs = df_class_feature["txId"].values
-    # df_edges = df_edges[(df_edges["txId1"].isin(known_txs)) & (df_edges["txId2"].isin(known_txs))]
-    
     #costruiamo il df con le edges formate solo dai nodi che conosciamo
     known_txs = df_class_feature["clId"].values
     df_edges = df_edges[(df_edges["clId1"].isin(known_txs)) & (df_edges["clId2"].isin(known_txs))]
 
     #rinomino le colonne delle etichette, ccLabel -> class
     df_class_feature = df_class_feature.rename(columns={'ccLabel': 'class'})
-
-    print("1\n" , df_class_feature)
-
 
     # Build indices for features and edge types
     # Prendo i valori univoci di clId e class, e in ordine crescente gli vado a dare un indice 
@@ -74,16 +33,11 @@ def load_data(data_path, noAgg=False):
     print("feauture_idx:\n" , features_idx) 
     print("class_idx:\n" , class_idx) 
 
-
-    print("2\n" , df_class_feature)
-
     # Apply index encoding to features
     # Queste due istruzioni non fanno altro che sostituire il valore originale
     # di clId/class con gli indici creati precedentemente
     df_class_feature["clId"] = df_class_feature["clId"].apply(lambda name: features_idx[name])
     df_class_feature["class"] = df_class_feature["class"].apply(lambda name: class_idx[name])
-
-    print("3\n" , df_class_feature)
 
     # Apply index encoding to edges
     # Qui fa la stessa identica cosa fatta al "df_class_feature" ma la applica al
@@ -91,9 +45,7 @@ def load_data(data_path, noAgg=False):
     df_edges["clId1"] = df_edges["clId1"].apply(lambda name: features_idx[name])
     df_edges["clId2"] = df_edges["clId2"].apply(lambda name: features_idx[name])
 
-    print("4\n" , df_class_feature)
-
-    #Inverto le colonne class e clId per bellezza
+    #Inverto le colonne class e clId
     df_class_feature = df_class_feature[['clId', 'class'] + list(df_class_feature.columns[2:])]
 
     print(df_class_feature)
@@ -103,15 +55,6 @@ def load_data(data_path, noAgg=False):
 
 
 def data_to_pyg(df_class_feature, df_edges):
-
-    # # Define PyTorch Geometric data structure with Pandas dataframe values
-    # edge_index = torch.tensor([df_edges["txId1"].values,
-    #                         df_edges["txId2"].values], dtype=torch.long)
-    # x = torch.tensor(df_class_feature.iloc[:, 3:].values, dtype=torch.float)
-    # y = torch.tensor(df_class_feature["class"].values, dtype=torch.long)
-
-    # data = Data(x=x, edge_index=edge_index, y=y)
-    # data = RandomNodeSplit(num_val=0.15, num_test=0.2)(data)
 
     # Define PyTorch Geometric data structure with Pandas dataframe values
     edge_index = torch.tensor([df_edges["clId1"].values,
